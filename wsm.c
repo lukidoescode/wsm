79ae2d4eab6db98d484a1f7f304feb17eb39a1bf
Lukas Wagner <lukaswagner93@googlemail.com>
2014-01-08 12:32:34 +0100

    Create wsm.c

---
+++ wsm.c
@@ -0,0 +1,13 @@
+/* Author: Lukas Wagner
+ * Created: 08.01.2014
+ * Description: this file holds the main function of the wayland session manager wsm
+ * 
+ * 
+ */
+
+
+
+
+int main (int argc, char** argv) {
+	return 0;
+}
