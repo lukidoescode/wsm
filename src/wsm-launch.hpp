d9266e91f3e164d49ee7e5c038e6d3bfec10ccc5
Lukas Wagner <lukaswagner93@googlemail.com>
2014-01-17 08:46:51 +0100

    Create wsm-launch.hpp

---
+++ src/wsm-launch.hpp
@@ -0,0 +1,20 @@
+/* wsm-launch.hpp
+*
+* Author: Lukas Wagner
+* Date of creation: Jan 17 2014
+*
+*/
+
+
+#ifndef __WSM_LAUNCH_HPP__
+#define __WSM_LAUNCH_HPP__
+
+class WsmLaunch {
+public:
+	
+private:
+	
+}
+
+
+#endif
